528df97837f14bd7ec5114eca6b76e2f588fd0b6
cosmicsagesling <a.johnmoses95@gmail.com>
2025-04-17 12:50:33 +0530

    loader fix

--- app/Components/Nala.tsx
+++ app/Components/Nala.tsx
@@ -1,6 +1,9 @@
+// components/NalaLogo.tsx
 "use client";
 
-import Lottie from "lottie-react";
+import dynamic from "next/dynamic";
+
+const Lottie = dynamic(() => import("lottie-react"), { ssr: false });
 import animationData from "@/public/Nala.json";
 
 export default function NalaLogo() {
